a391a3643e9d0def7eb2fa3ecc90d35402bb04ce
nishgowda3 <nishgowda3@gmail.com>
2016-02-21 18:48:58 +0000

    Added files via upload

---
+++ lib/usagewatch.rb
@@ -0,0 +1,10 @@
+require "usagewatch/version"
+
+os = RUBY_PLATFORM
+text =  "Unsupported OS! If you are using a Operating System that is not Linux, please try out the Gem usagewatch_ext by Ruben Espinosa" 
+
+if os.include? "linux"
+  require "usagewatch/linux"
+else
+   puts text
+end
